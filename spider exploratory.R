e2c9f06cdf4baa9c0a51395c4bfb85134161f486
Vitor Rios <vrios81@gmail.com>
2018-08-01 11:25:47 -0300

    minor updates to plot and path

--- spider exploratory.R
+++ spider exploratory.R
@@ -3,7 +3,7 @@ library(viridis)
 
 
 #linear = read.table("output.csv", header=F,sep=",")
-path = "C:\\Users\\Vrios\\Dropbox\\repositorios\\space-spiders\\output"
+
 
 # files.linear <- list.files(path=path,full.names = T,pattern = "linear")
 # tmp.linear=  lapply(files.linear, read.table,  header=F,sep=",")
@@ -35,21 +35,27 @@ path = "C:\\Users\\Vrios\\Dropbox\\repositorios\\space-spiders\\output"
 # sd.linear= aggregate(x = linear$action_prob, by=list(linear$danger_level), FUN= sd)
 # plot(sd.linear)
 
-
+path = "C:\\Users\\Vrios\\Dropbox\\repositorios\\space-spiders\\output"
 files <- list.files(path=path,full.names = T,pattern = ".csv")
 tmp =  lapply(files, read.table,  header=F,sep=",")
 spiders = do.call(rbind, tmp)
 str(spiders)
 summary(spiders)
-colnames(spiders)=c("ticks" , "who" , "action_prob" , "danger_level", "danger_shape", "replicate_number", "k", "x0","a","b")#,"par")
+colnames(spiders)=c("ticks" , "who" , "action_prob" , "danger_level", "danger_shape","distribution", "replicate_number", "k", "x0","a","b")#,"par")
 spiders$danger_shape=as.factor(spiders$danger_shape)
+spiders$distribution=as.factor(spiders$distribution)
 str(spiders)
+
+l=levels(spiders$danger_shape)
+l   
+spiders$danger_shape<- factor(spiders$danger_shape, levels = c("k=-20 x0=0.9", "k=-20 x0=0.7", "k=-20 x0=0.5","k=-10 x0=0.9", "k=-10 x0=0.7","k=-5 x0=0.9" ))
+
 p <- ggplot(spiders, aes(x=spiders$danger_shape, y=spiders$action_prob)) + 
   scale_y_continuous(limits = c(0, 1))+
  # geom_boxplot(notch=TRUE) +
  # scale_fill_viridis() + 
-  geom_violin(adjust = 1.5) + #ggtitle("several sigmoid")+
-#   geom_boxplot(notch=TRUE) +
+  #geom_violin(adjust = 1.5) + #ggtitle("several sigmoid")+
+   geom_boxplot(notch=TRUE) +
   theme(
     axis.line.y =  element_line(colour = "black"),
     axis.line.x =  element_line(colour = "black"),
@@ -59,10 +65,14 @@ p <- ggplot(spiders, aes(x=spiders$danger_shape, y=spiders$action_prob)) +
     panel.grid.minor = element_blank(),
     panel.border = element_blank(),
     panel.background = element_blank()) +
-  labs(x="Estrutura da teia",y="ìndice de Personalidade",color="Pers.") #+   scale_fill_viridis()
+  labs(x="Estrutura da teia",y="Indice de Personalidade",color="Pers.") #+   scale_fill_viridis()
 x11();p
 ggsave("novas.jpg",device = "jpg")
 
+library("ggpubr")
+x11();ggboxplot(spiders, x  = "distribution", y = "action_prob", color="danger_shape")
+x11();ggviolin(spiders, x  = "distribution", y = "action_prob", color="danger_shape")
+,add = "boxplot")
 
 modelo=lm(spiders$action_prob ~ spiders$danger_type)
 mod= aov(spiders$action_prob ~ spiders$danger_type)
